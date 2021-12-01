fb00e55334847078b3eabdd6acc4ae646369995b
fady gendy <fady@debian.home>
2021-12-01 21:28:24 +0200

    created core commands for cron job

---
+++ app/Console/Commands/sendNewTrades.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class sendNewTrades extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:sendnewtrades';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sending new trading information to the admin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        echo "new trades sent successfully.\n";
+        return Command::SUCCESS;
+    }
+}
